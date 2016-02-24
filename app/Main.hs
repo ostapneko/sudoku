040729677964cde980fd1ae6ac84318fd56dbadb
Thomas Franquelin <ostapneko@gmail.com>
2016-02-24 12:19:05 +0000

    Add minimal shell

--- app/Main.hs
+++ app/Main.hs
@@ -5,10 +5,16 @@ module Main where
 import Solver
 import qualified Data.Vector as V
 import Data.Vector ((!))
+import System.Environment
 
 main :: IO ()
 main = do
-  let str = "........49.1.6.5....8....9.8..4.37.6....1....54...718.....39..7....4..5....7..81."
-  let grid = parse str
-      solved = solve grid
-  print $ solved
+  args <- getArgs
+  case args of
+    [str] -> do
+      let grid = parse str
+          solved = solve grid
+      putStrLn $ foldr (\w s -> w ++ s) "" $ map show $ V.toList $ V.map val solved
+    _ -> do
+      prgName <- getProgName
+      putStrLn $ "Usage: " ++ prgName ++ " <grid>\n where grid is in a format such as\n ........49.1.6.5....8....9.8..4.37.6....1....54...718.....39..7....4..5....7..81."
